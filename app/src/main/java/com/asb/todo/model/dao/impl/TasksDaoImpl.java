071a30acf1e1790cb022cc5920ee27e879bd9e4f
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2015-06-06 15:03:32 +0530

    Adding Dao Implementation

--- app/src/main/java/com/asb/todo/model/dao/impl/TasksDaoImpl.java
+++ app/src/main/java/com/asb/todo/model/dao/impl/TasksDaoImpl.java
@@ -1,11 +1,14 @@
 package com.asb.todo.model.dao.impl;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
 
 import com.asb.todo.model.dao.TasksDao;
 import com.asb.todo.model.entities.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +16,9 @@ import java.util.List;
  */
 public class TasksDaoImpl implements TasksDao {
 
+    public static final int FALSE = 0;
+    public static final int TRUE = 1;
+    private static final String FIND_BY_ID = COL_ID + " =  ?";
     private SQLiteDatabase database;
 
     public TasksDaoImpl(SQLiteDatabase database) {
@@ -21,42 +27,116 @@ public class TasksDaoImpl implements TasksDao {
 
     @Override
     public void addTask(Task task) {
-
+        ContentValues values = getContentValuesFromTask(task);
+        database.insert(TABLE_NAME, null, values);
     }
 
     @Override
     public void addTasks(List<Task> tasks) {
-
+        database.beginTransaction();
+        try {
+            for (Task task : tasks) {
+                addTask(task);
+            }
+            database.setTransactionSuccessful();
+        } finally {
+            database.endTransaction();
+        }
     }
 
     @Override
     public Task getTaskById(long id) {
-        return null;
+        String[] selectionArgs = {String.valueOf(id)};
+        Cursor cursor =
+                database.query(TABLE_NAME, null, FIND_BY_ID, selectionArgs, null, null, null);
+        Task task = null;
+        if (cursor.moveToFirst()) {
+            task = loadTaskFromCursor(cursor);
+        }
+        return task;
     }
 
     @Override
     public Task updateTask(Task task) {
-        return null;
+        ContentValues values = getContentValuesFromTask(task);
+        String[] selectionArgs = {String.valueOf(task.getId())};
+        database.update(TABLE_NAME, values, FIND_BY_ID, selectionArgs);
+        return task;
     }
 
     @Override
     public List<Task> updateTasks(List<Task> tasks) {
-        return null;
+        database.beginTransaction();
+        try {
+            for (Task task : tasks) {
+                updateTask(task);
+            }
+            database.setTransactionSuccessful();
+        } finally {
+            database.endTransaction();
+        }
+        return tasks;
     }
 
     @Override
     public Task deleteTask(long id) {
-        return null;
+        Task task = getTaskById(id);
+        String[] selectionArgs = {String.valueOf(id)};
+        database.delete(TABLE_NAME, FIND_BY_ID, selectionArgs);
+        return task;
     }
 
     @Override
     public List<Task> deleteTasks(List<Long> ids) {
-        return null;
+        List<Task> tasks = new ArrayList<>(ids.size());
+        database.beginTransaction();
+        try {
+            for (long id : ids) {
+                Task task = getTaskById(id);
+                tasks.add(task);
+                deleteTask(id);
+            }
+            database.setTransactionSuccessful();
+        } finally {
+            database.endTransaction();
+        }
+        return tasks;
     }
 
     @Override
     public Cursor getTasks(String selection, String[] selectionArgs, String groupBy,
                            String orderBy) {
-        return null;
+        return database.query(TABLE_NAME, null, selection, selectionArgs, groupBy, null, orderBy);
     }
+
+    private ContentValues getContentValuesFromTask(Task task) {
+        ContentValues values = new ContentValues();
+        values.put(COL_NAME, task.getName());
+        if (!TextUtils.isEmpty(task.getDescription())) {
+            values.put(COL_DESCRIPTION, task.getDescription());
+        }
+        values.put(COL_START_TIME, task.getStartTime());
+        values.put(COL_END_TIME, task.getEndTime());
+        if (task.isCompleted()) {
+            values.put(COL_IS_COMPLETED, TRUE);
+        }
+        else {
+            values.put(COL_IS_COMPLETED, FALSE);
+        }
+        return values;
+    }
+
+    private Task loadTaskFromCursor(Cursor cursor) {
+        Task task;
+        Task.Builder builder = new Task.Builder();
+        builder.setId(cursor.getLong(cursor.getColumnIndex(COL_ID)))
+                .setName(cursor.getString(cursor.getColumnIndex(COL_NAME)))
+                .setDescription(cursor.getString(cursor.getColumnIndex(COL_DESCRIPTION)))
+                .setStartTime(cursor.getLong(cursor.getColumnIndex(COL_START_TIME)))
+                .setEndTime(cursor.getLong(cursor.getColumnIndex(COL_END_TIME)))
+                .setCompleted((TRUE == cursor.getLong(cursor.getColumnIndex(COL_IS_COMPLETED))));
+        task = builder.build();
+        return task;
+    }
+
 }
